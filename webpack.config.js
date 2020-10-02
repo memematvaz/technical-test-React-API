ce61400248e0ef7e91d832af3fd28d6fd2445703
memematvaz <memematvaz@gmail.com>
2020-10-02 19:20:10 +0200

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 const path = require('path');
+const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/frontend/index.js',
+    entry: ['./src/frontend/index.js', 'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000&reload=true'
+  ],
+    mode: 'development',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
@@ -49,6 +52,8 @@ module.exports = {
         ]
     },
     plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+
         new HtmlWebPackPlugin({
             template: './public/index.html',
             filename: './index.html'
